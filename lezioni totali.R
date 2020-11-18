60715dad880dc210fc4309b0c1ba6b40d5ac130d
snowy2710 <74658096+snowy2710@users.noreply.github.com>
2020-11-18 22:00:30 +0100

    Add files via upload

---
+++ lezioni totali.R
@@ -0,0 +1,15 @@
+#esempi
+
+show(cars)
+#questo comando "show" mostra le variabili e i valori di un dataset"cars"
+
+hist(cars$dist)
+#"hist" msotra un grafico, istogramma, del dataset "cars" in funzione della variabile "dist"
+
+plot(cars)
+plot(cars$speed, cars$dist)
+#"plot" mostra grafico a puntini in relazione delle due variabili x(speed), e y(dist)
+
+help(show)
+help(hist)
+#usare comando "help" per far comparire la spiegazione del dato
